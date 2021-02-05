fd77da53e5b36a2cccbb36be37d8532e8fe42891
Kota Kokado <kouta-k@mbm.nifty.com>
2021-02-05 13:22:25 +0900

    generate SECRET_KEY in every time

--- project/pro1/pro1/settings.py
+++ project/pro1/pro1/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'mn^y968y79fcb9d+8m!n%gd-0*a$h(a4vk&4o2_^-)7)*xy4z4'
+from .secret_key import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
