ba0655d9dfaab82e465a8deb4f4da41fe2a3e16a
kokardy <mgc1028@gmail.com>
2021-02-06 16:07:09 +0900

    secret keyのリフレッシュとsecret_key.pyを.gitignoreに登録

--- project/pro1/pro1/secret_key.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-#SECRET_KEY = 'amq&szxm#%!bq7)k8&ku5+#u-^3b*kaejgx)d4nbiujz%m#8x!'
-
-if "SECRET_KEY" not in locals():
-    try:
-        from django.core.management.utils import get_random_secret_key
-        SECRET_KEY = get_random_secret_key()
