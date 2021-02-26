b7468645d3f43ae12b60ccaa0dbd213620f3938b
kokardy <mgc1028@gmail.com>
2021-02-26 19:05:50 +0900

    graphene_django

--- asset/project/project/settings.py
+++ asset/project/project/settings.py
@@ -10,7 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import project
+from .secret_key import SECRET_KEY
 from pathlib import Path
+
+GRAPHENE = {
+    'SCHEMA': 'project.schema.schema',
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,14 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from .secret_key import SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-import project
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +42,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # GraphQL
+    'graphene_django',
+
+    # myapp
     'app1.apps.App1Config',
 ]
 
@@ -77,8 +85,8 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
 
         'ENGINE': 'django.db.backends.postgresql',
         'HOST': 'postgres',
