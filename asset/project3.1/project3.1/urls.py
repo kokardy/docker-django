3d7d487b0054bd4e8d59005a4c07d453fef6c46a
kokardy <mgc1028@gmail.com>
2021-04-08 00:13:41 +0900

    app config

--- asset/project3.1/project3.1/urls.py
+++ asset/project3.1/project3.1/urls.py
@@ -1,7 +1,7 @@
 """project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from graphene_django.views import GraphQLView
+
+from django.views.decorators.csrf import csrf_exempt
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
