e7f3ff7882b531446bd7b23ca4839afb6994c700
kokardy <2720900+kokardy@users.noreply.github.com>
2021-04-08 18:56:31 +0900

    secretkey

--- asset/project/project/settings.py
+++ asset/project/project/settings.py
@@ -19,8 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+from secret_key import SECRET_KEY
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8@w+u=hq0tro1@2djv(hp)j7-ddp!dl2wgvnf2z%6&i6_+mv5%'
+# SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
