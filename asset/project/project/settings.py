2e8918d249b34c0e612a8dc04ff91f2d11d55803
kokardy <mgc1028@gmail.com>
2021-07-17 17:16:02 +0900

    rest api

--- asset/project/project/settings.py
+++ asset/project/project/settings.py
@@ -28,6 +28,14 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# rest_framework
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Application definition
 
@@ -38,6 +46,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'app1.apps.App1Config',
+
 ]
 
 MIDDLEWARE = [
