5bf0253c2295191b163e2b0019f9c7bc27cc1826
kokardy <mgc1028@gmail.com>
2022-02-23 18:05:02 +0900

    tmp

--- asset/project2/app1/schema.py
+++ asset/project2/app1/schema.py
@@ -4,32 +4,32 @@ from graphene_django.filter import DjangoFilterConnectionField
 
 from .models import Person, Book
 
+
 class PersonType(DjangoObjectType):
     class Meta:
         model = Person
-        filter_fields = ('id', 'name', 'age')
+        filter_fields = ("id", "name", "age")
         interfaces = (graphene.relay.Node,)
+
 
 class BookType(DjangoObjectType):
     class Meta:
         model = Book
 
+
 class Query(graphene.ObjectType):
-    person = graphene.Field(PersonType, id=graphene.Int())
-    #persons = graphene.List(PersonType)
-    persons = DjangoFilterConnectionField(PersonType)
+    persons = graphene.Field(PersonType, id=graphene.Int())
     books = graphene.List(BookType)
 
-    #@graphene.resolve_only_args
-    #def resolve_persons(self):
-    #    return Person.objects.all()
+    goodbye = graphene.String()
 
     @graphene.resolve_only_args
     def resolve_person(self, id):
         return Person.objects.filter(pk=id).first()
 
-    
     @graphene.resolve_only_args
     def resolve_books(self):
         return Book.objects.all()
-    
+
+    def resolve_goodbye(root, info):
+        return "say Ya"
